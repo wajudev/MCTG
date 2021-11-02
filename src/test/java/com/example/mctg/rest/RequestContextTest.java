75a33c60bdecb80a453af9e3fc797143a5572d46
Waju <lanre.ajibua@gmail.com>
2021-11-02 18:03:10 +0100

    Added test for RestService and RequestContext

---
+++ src/test/java/com/example/mctg/rest/RequestContextTest.java
@@ -0,0 +1,30 @@
+package com.example.mctg.rest;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.net.Socket;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class RequestContextTest {
+    @Mock
+    Socket socketMock;
+
+    @SneakyThrows
+    @Test
+    @DisplayName("Handle a Socket within RequestContext")
+    void testSocketHandler(){}
+}
--- src/test/java/com/example/mctg/rest/RestServiceTest.java
+++ src/test/java/com/example/mctg/rest/RestServiceTest.java
@@ -1,7 +1,34 @@
 package com.example.mctg.rest;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RestServiceTest {
+    RestService restService1;
+    RestService restService2;
 
+    @BeforeEach
+    void beforeEach(){
+        restService1 = new RestService(0); // Any free port
+        restService2 = new RestService(0);
+    }
+
+    @Test
+    @DisplayName("The RestService class should implement the Runnable interface.")
+    void testRestService__runnable() {
+        Thread t1 = new Thread(restService1);
+        Thread t2 = new Thread(restService2);
+
+        assertNull(restService1.getListener());
+        assertNull(restService2.getListener());
+
+        t1.start();
+        t2.start();
+
+        t1.interrupt();
+        t2.interrupt();
+    }
 }
